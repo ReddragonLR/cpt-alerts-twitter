917382160b0626ea468b92673195ac3d92161450
Lishen Ramsudh <lishenramsudh@Lishens-MacBook-Pro.local>
2019-08-15 01:01:06 +0200

    Code cleanup

--- src/twitter_client.py
+++ src/twitter_client.py
@@ -21,24 +21,6 @@ class TwitterClient():
     def get_twitter_client_api(self):
         return self.twitter_client
 
-    def get_user_timeline_tweets(self, num_tweets):
-        tweets = []
-        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
-            tweets.append(tweet)
-        return tweets
-
-    def get_friend_list(self, num_friends):
-        friend_list = []
-        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
-            friend_list.append(friend)
-        return friend_list
-
-    def get_home_timeline_tweets(self, num_tweets):
-        home_timeline_tweets = []
-        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
-            home_timeline_tweets.append(tweet)
-        return home_timeline_tweets
-
 
 # # # # TWITTER AUTHENTICATER # # # #
 class TwitterAuthenticator():
